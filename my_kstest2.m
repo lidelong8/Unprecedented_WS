2e3514308459af68577e6b79553b2c9cfdbb6f90
lidelong8 <133221050+lidelong8@users.noreply.github.com>
2025-02-16 19:43:43 +0800

    Add files via upload

---
+++ my_kstest2.m
@@ -0,0 +1,96 @@
+function [H, pValue] = my_kstest2(x1, x2)
+%KSTEST2 Two-sample Kolmogorov-Smirnov goodness-of-fit hypothesis test.
+%   H = KSTEST2(X1,X2,ALPHA,TYPE) performs a Kolmogorov-Smirnov (K-S) test 
+%   to determine if independent random samples, X1 and X2, are drawn from 
+%   the same underlying continuous population. ALPHA and TYPE are optional
+%   scalar inputs: ALPHA is the desired significance level (default = 0.05); 
+%   TYPE indicates the type of test (default = 'unequal'). H indicates the
+%   result of the hypothesis test:
+%      H = 0 => Do not reject the null hypothesis at significance level ALPHA.
+%      H = 1 => Reject the null hypothesis at significance level ALPHA.
+% 
+%   X1 and X2 are vectors of lengths N1 and N2, respectively, and represent
+%   random samples from some underlying distribution(s). Missing
+%   observations, indicated by NaNs (Not-a-Number), are ignored.
+%
+%   [H,P] = KSTEST2(...) also returns the asymptotic P-value P.
+%
+%   [H,P,KSSTAT] = KSTEST2(...) also returns the K-S test statistic KSSTAT
+%   defined above for the test type indicated by TYPE.
+%
+%   The asymptotic P-value becomes very accurate for large sample sizes, and
+%   is believed to be reasonably accurate for sample sizes N1 and N2 such 
+%   that (N1*N2)/(N1 + N2) >= 4.
+
+
+
+
+% Remove missing observations indicated by NaN's, and 
+% ensure that valid observations remain.
+%
+
+x1  =  x1(~isnan(x1));
+x2  =  x2(~isnan(x2));
+x1  =  x1(:);
+x2  =  x2(:);
+
+
+%
+% Ensure the significance level, ALPHA, is a scalar 
+% between 0 and 1 and set default if necessary.
+%
+alpha  =  0.05;
+%
+% Ensure the type-of-test indicator, TYPE, is a scalar integer from 
+% the allowable set, and set default if necessary.
+%
+
+%
+% Calculate F1(x) and F2(x), the empirical (i.e., sample) CDFs.
+%
+
+binEdges    =  [-inf ; sort([x1;x2]) ; inf];
+
+binCounts1  =  histc (x1 , binEdges, 1);
+binCounts2  =  histc (x2 , binEdges, 1);
+
+sumCounts1  =  cumsum(binCounts1)./sum(binCounts1);
+sumCounts2  =  cumsum(binCounts2)./sum(binCounts2);
+
+sampleCDF1  =  sumCounts1(1:end-1);
+sampleCDF2  =  sumCounts2(1:end-1);
+
+%
+% Compute the test statistic of interest.
+%
+
+tail =0;
+deltaCDF  =  abs(sampleCDF1 - sampleCDF2); %  2-sided test: T = max|F1(x) - F2(x)|.
+
+KSstatistic   =  max(deltaCDF);
+
+%
+% Compute the asymptotic P-value approximation and accept or
+% reject the null hypothesis on the basis of the P-value.
+%
+
+n1     =  length(x1);
+n2     =  length(x2);
+n      =  n1 * n2 /(n1 + n2);
+lambda =  max((sqrt(n) + 0.12 + 0.11/sqrt(n)) * KSstatistic , 0);
+
+if tail ~= 0        % 1-sided test.
+
+   pValue  =  exp(-2 * lambda * lambda);
+
+else                % 2-sided test (default).
+%
+%  Use the asymptotic Q-function to approximate the 2-sided P-value.
+%
+   j       =  (1:101)';
+   pValue  =  2 * sum((-1).^(j-1).*exp(-2*lambda*lambda*j.^2));
+   pValue  =  min(max(pValue, 0), 1);
+
+end
+
+H  =  (alpha >= pValue);
